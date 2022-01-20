1c1cc3b107fb54d128226268f3765546117352e5
Jan Pierre <gamunaxx@gmail.com>
2022-01-19 19:31:07 -0500

    codigo para saltar

--- Assets/scripts/NewBehaviourScript.cs
+++ Assets/scripts/NewBehaviourScript.cs
@@ -6,10 +6,17 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 1.0f;
     public float rotationSpeed = 1.0f;
+    public float JumpForce = 1.0f;
+
+    private Rigidbody Physics;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Physics = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,7 +26,12 @@ public class NewBehaviourScript : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(horizontal, 0.0f, vertical) * Time.deltaTime * speed);
+
         float rotationY = Input.GetAxis("Mouse X");
         transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * rotationSpeed, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Physics.AddForce(new Vector3(0, 1, 0), ForceMode.Impulse);
+        }
     }
 }
